8eb15f82cc0b8ac67879f148119667f292bf28bc
Uriel Larrea <kuroshiromugen@gmail.com>
2025-07-22 11:01:20 -0300

    Minimize to tray, project migrated to NET 8, placeholder for icons

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
 ## July, 2025
+###22/07
+- Project rebuilt on .NET 8 (previous was .Net Framework 4.7.2)
+- SQLite package installed
+- Now it's possible to minimize to system tray and open / close from there.
+- Added placeholder icons for both the app and the options from the system tray context menu strip.
 ### 05/07 
 - Created the repository, made the readme, changelog.md, created the default solution in Visual Studio for a WPF App with C# and .NET Framework.
--- KakitoriApp/MainWindow.xaml.cs
+++ KakitoriApp/MainWindow.xaml.cs
@@ -1,28 +1,95 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace KakitoriApp
 {
-    /// <summary>
-    /// Lógica de interacción para MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private System.Windows.Forms.NotifyIcon _notifyIcon;
+        private bool _forceClose = false;
+
         public MainWindow()
         {
             InitializeComponent();
+            InitializeTrayIcon();
+            this.Visibility = Visibility.Hidden;
+        }
+
+        private void InitializeTrayIcon()
+        {
+            _notifyIcon = new System.Windows.Forms.NotifyIcon
+            {
+                Text = "Kakitori",
+                Icon = new System.Drawing.Icon("TheAppIcon.ico"),
+                Visible = true
+            };
+
+            var contextMenu = new System.Windows.Forms.ContextMenuStrip();
+
+            var menuItemOpen = new System.Windows.Forms.ToolStripMenuItem("Open");
+            menuItemOpen.Image = Image.FromFile("TheAppIcon.ico");
+
+            var menuItemClose = new System.Windows.Forms.ToolStripMenuItem("Close");
+            menuItemClose.Image = Image.FromFile("TheAppIcon.ico");
+
+            menuItemOpen.Click += NotifyIcon_Open;
+            menuItemClose.Click += NotifyIcon_Close;
+
+            contextMenu.Items.Add(menuItemOpen);
+            contextMenu.Items.Add(menuItemClose);
+
+            _notifyIcon.ContextMenuStrip = contextMenu;
+            _notifyIcon.DoubleClick += NotifyIcon_DoubleClick;
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            if (!_forceClose)
+            {
+                e.Cancel = true;
+                this.Hide();
+            }
+            else
+            {
+                _notifyIcon.Dispose();
+                _notifyIcon = null;
+            }
+
+            base.OnClosing(e);
+        }
+
+        protected override void OnStateChanged(EventArgs e)
+        {
+            if (WindowState == WindowState.Minimized)
+            {
+                this.Hide();
+            }
+
+            base.OnStateChanged(e);
+        }
+
+        private void OpenFromTray()
+        {
+            this.Show();
+            this.WindowState = WindowState.Normal;
+            this.Activate();
+        }
+
+        private void NotifyIcon_Open(object sender, EventArgs e)
+        {
+            OpenFromTray();
+        }
+
+        private void NotifyIcon_Close(object sender, EventArgs e)
+        {
+            _forceClose = true;
+            this.Close();
+        }
+
+        private void NotifyIcon_DoubleClick(object sender, EventArgs e)
+        {
+            OpenFromTray();
         }
     }
 }
